3a505fc0e97fc2da0851d5fc47a57207d34b3dbb
Kevin <knphillips01@gmail.com>
2020-03-20 10:23:01 -0500

    first commit - downloaded dependencies and setup folder structure

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createstore, applymiddleware } from 'redux'
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+
+ReactDOM.render(
+  <Provider store={store}>
+      <App />
+  </Provider>
+, document.getElementById("root"));
